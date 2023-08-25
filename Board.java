3cb53b6f2c1677a66a47aaea309dc1d920ce6a86
Jay Santamaria <102671405+jtotheayy@users.noreply.github.com>
2023-08-25 16:52:01 -0400

    Ultimate Tic-Tac-Toe Game 2-Player
    This is my Ultimate tic-tac-toe game that uses 2-player.
    This game is a large tic-tac-toe board, with smaller tic-tac-toe boards in each of the squares. Turns are taken in
    the small boards. When a player makes a turn in the small tic-tac-toe board, then the next player plays in the small board that corresponds to the square the previous player played. For Example: Player "X" plays their turn in the "middle-right" board, and in that board they play the square "top-middle". Now Player "O" is forced to play in the "top-middle" board and can click on any of the squares in that board. If,"X" plays a square, where the corresponding board is already won, then "O" can play their turn on any square, from any un-won boards. Those are the rules, the goal is to create a three-in-a-row pattern in the large board and win the game.
    Good Luck

---
+++ Board.java
@@ -0,0 +1,179 @@
+import javax.swing.JButton;
+import java.util.Scanner;
+
+
+
+public class Board {
+    String symbol = "";
+    Board board[][] = new Board[3][3];
+
+
+    String PlayerX = "X";
+    String PlayerO = "O";
+
+    public static Boolean checkWinner(Board boardNum) {
+        Board[][] thisBoard = boardNum.getBoard();
+        String cases[] = new String[8];
+        cases[0] = thisBoard[0][0].getSym() + thisBoard[0][1].getSym() + thisBoard[0][2].getSym();
+        cases[1] = thisBoard[1][0].getSym() + thisBoard[1][1].getSym() + thisBoard[1][2].getSym();
+        cases[2] = thisBoard[2][0].getSym() + thisBoard[2][1].getSym() + thisBoard[2][2].getSym();
+        cases[3] = thisBoard[0][0].getSym() + thisBoard[1][0].getSym() + thisBoard[2][0].getSym();
+        cases[4] = thisBoard[0][1].getSym() + thisBoard[1][1].getSym() + thisBoard[2][1].getSym();
+        cases[5] = thisBoard[0][2].getSym() + thisBoard[1][2].getSym() + thisBoard[2][2].getSym();
+        cases[6] = thisBoard[0][0].getSym() + thisBoard[1][1].getSym() + thisBoard[2][2].getSym();
+        cases[7] = thisBoard[0][2].getSym() + thisBoard[1][1].getSym() + thisBoard[2][0].getSym();
+
+
+        for (int i = 0; i < 8; i++) {
+            if (cases[i].equals("XXX") || cases[i].equals("OOO")) {
+                return true;
+            }
+        }
+        return false;
+
+    }
+//Set the symbol "X" or "O" on the board's square
+    public void setSym(String i) {
+        this.symbol = i;
+    }
+
+    public String getSym() {
+        if (this.symbol == null) {
+            return "emp";
+        }
+
+        return symbol;
+    }
+
+
+
+    public Board[][] getBoard() {
+        return this.board;
+    }
+
+    public Board getBoard(int num) {
+        int row = (num - 1) / 3;
+        int col = (num - 1) % 3;
+        return this.board[row][col];
+    }
+
+
+//prints the game board
+    public static void printBoard(Board game) {
+
+        Board[][] thisBoard = game.getBoard();
+
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                Board[][] innerBoard = thisBoard[i][j].getBoard();
+                for (int x = 0; x < 3; x++) {
+                    for (int y = 0; y < 3; y++) {
+                        System.out.print(innerBoard[x][y].getSym());
+                    }
+                    System.out.println();
+                }
+                System.out.println("_______________________________");
+
+            }
+            // Move to the next row
+        }
+
+    }
+
+//initialized the large game board
+    public static void createBoard(Board[][] main) {
+
+
+        Board board1 = new Board();
+        main[0][0] = board1;
+        Board board2 = new Board();
+        main[0][1] = board2;
+        Board board3 = new Board();
+        main[0][2] = board3;
+        Board board4 = new Board();
+        main[1][0] = board4;
+        Board board5 = new Board();
+        main[1][1] = board5;
+        Board board6 = new Board();
+        main[1][2] = board6;
+        Board board7 = new Board();
+        main[2][0] = board7;
+        Board board8 = new Board();
+        main[2][1] = board8;
+        Board board9 = new Board();
+        main[2][2] = board9;
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                main[i][j].setSym("-");
+            }
+        }
+
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                setInitial(main[i][j]);
+
+            }
+        }
+    }
+
+//initializes the symbols on the game board
+    public static void setInitial(Board board) {
+        Board[][] innerBoard = board.getBoard();
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                innerBoard[i][j] = new Board();
+                innerBoard[i][j].setSym("-");
+            }
+        }
+    }
+
+//game functionality
+    public static boolean playGame(int board, int row, int col, String currentPlayer, Board tttBoard, String Focus) {
+        int focusedBoard = -1;
+        try {
+            focusedBoard = Integer.parseInt(Focus);
+            System.out.println("Can only place a marker on board: " + focusedBoard);
+
+        } catch (Exception e) {
+            if (Focus.equals("A")) {
+                System.out.println("All boards are available to place a marker on :)");
+            }
+        }
+        if (board == focusedBoard || Focus.equals("A")) {
+
+            int boardNumber = board + 1;
+            int boxNumber = row * 3 + col + 1;
+
+            Board targetBoard = tttBoard.getBoard(boardNumber);
+
+            if (targetBoard != null) {
+                Board targetBox = targetBoard.getBoard(boxNumber);
+                if (targetBox != null && targetBox.getSym().equals("-")) {
+                    targetBox.setSym(currentPlayer);
+                    //printBoard(tttBoard);
+
+                    if (checkWinner(targetBoard)) {
+                        System.out.println(currentPlayer + "WINS!!! board: " + targetBoard);
+                    }
+                    return true;
+
+                } else {
+                    System.out.println("Box is already taken. Try again.");
+                    return false;
+                }
+            } else {
+                System.out.println("Invalid board number. Try again.");
+                return false;
+            }
+        } else {
+            System.out.println("Invalid board number. Try again.");
+            return false;
+        }
+    }
+
+
+
+}
---
+++ UltTTT.java
@@ -0,0 +1,273 @@
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.Scanner;
+
+import javax.swing.*;
+import javax.swing.border.Border;
+
+//Created By Jay Santamaria
+//Ultimate Tic Tac Toe Game
+//Rules:
+/*This game is a large tic-tac-toe board, with smaller tic-tac-toe boards in each of the squares. Turns are taken in
+ * the small boards. When a player makes a turn in the small tic-tac-toe board, then the next player plays in the small board
+ * that corresponds to the square the previous player played. For Example: Player "X" plays their turn in the "middle-right" board,
+ * and in that board they play the square "top-middle". Now Player "O" is forced to play in the "top-middle" board and can click on 
+ * any of the squares in that board. If,"X" plays a square, where the corresponding board is already won, then "O" can play their turn
+ * on any square, from any un-won boards. Those are the rules, the goal is to create a three-in-a-row pattern in the large board
+ * and win the game.
+ * Good Luck
+ */
+public class UltTTT extends Board {
+    static String currentPlayer = "X";
+
+    //A, 1, 2, 3, 4, 5, 6, 7, 8, 9
+    static String focusedBoard = "A";
+    static boolean[] wonBoards = {
+        false,
+        false,
+        false,
+        false,
+        false,
+        false,
+        false,
+        false,
+        false
+    };
+    static Board wholeBoardCheck = new Board(); 
+
+// Run the game
+    public static void main(String[] args) {
+
+        display();
+
+    }
+//Changes the current player of the game
+    public static void changePlayer() {
+        currentPlayer = (currentPlayer.equals("X")) ? "O" : "X";
+    }
+
+//Changes the focus on the board, meaning it only sets the boards that are allowed for play by the rules of the game. All other boards are unplayable
+    public static void changeFocus(int focus, JFrame wholeBoard) {
+        Border unfocusBorder = BorderFactory.createDashedBorder(Color.BLACK, 4, 4, 4, false);
+
+        for (int i = 0; i < 9; i++) {
+            if (!wonBoards[i]) {
+                JPanel unFocusedPanel = (JPanel) wholeBoard.getContentPane().getComponent(i);
+                unFocusedPanel.setBorder(unfocusBorder);
+            }
+        }
+
+        focusedBoard = focus + "";
+        Border focusBorder = BorderFactory.createLineBorder(Color.GREEN, 6, false);
+
+        if (focusedBoard.equals("-1") || wonBoards[focus]) {
+            for (int i = 0; i < 9; i++) {
+                if (!wonBoards[i]) {
+                    JPanel focusPanel = (JPanel) wholeBoard.getContentPane().getComponent(i);
+                    focusPanel.setBorder(focusBorder);
+                }
+            }
+            focusedBoard = "A";
+        } else {
+            JPanel focusPanel = (JPanel) wholeBoard.getContentPane().getComponent(focus);
+            focusPanel.setBorder(focusBorder);
+        }
+    }
+
+//Marks the player's turn onto the board
+    public static void markBoard(int board, int row, int col, JFrame wholeBoard) {
+
+        JPanel boardPanel = (JPanel) wholeBoard.getContentPane().getComponent(board);
+        JButton clickedButton = (JButton) boardPanel.getComponent(row * 3 + col);
+        Font symbol = new Font(clickedButton.getFont().getName(), Font.PLAIN, 64);
+
+        if (clickedButton.getText().equals("-")) {
+            clickedButton.setText(currentPlayer);
+            clickedButton.setBackground(Color.BLACK);
+            if (currentPlayer.equals("X")) {
+                clickedButton.setBackground(Color.RED);
+                clickedButton.setFont(symbol);
+            } else {
+                clickedButton.setBackground(Color.BLUE);
+                clickedButton.setFont(symbol);
+
+            }
+
+            clickedButton.setEnabled(false);
+
+
+        }
+
+    }
+//Marks the player's won boards, if any, and tracks it
+    public static void markWonBoard(int board, JFrame wholeBoard) {
+        JPanel boardPanel = (JPanel) wholeBoard.getContentPane().getComponent(board);
+
+        if (wonBoards[board]) {
+
+            if (currentPlayer.equals("X")) {
+                JButton b1 = (JButton) boardPanel.getComponent(0);
+                b1.setBackground(Color.RED);
+                JButton b2 = (JButton) boardPanel.getComponent(1);
+                b2.setBackground(Color.BLACK);
+                JButton b3 = (JButton) boardPanel.getComponent(2);
+                b3.setBackground(Color.RED);
+                JButton b4 = (JButton) boardPanel.getComponent(3);
+                b4.setBackground(Color.BLACK);
+                JButton b5 = (JButton) boardPanel.getComponent(4);
+                b5.setBackground(Color.RED);
+                JButton b6 = (JButton) boardPanel.getComponent(5);
+                b6.setBackground(Color.BLACK);
+                JButton b7 = (JButton) boardPanel.getComponent(6);
+                b7.setBackground(Color.RED);
+                JButton b8 = (JButton) boardPanel.getComponent(7);
+                b8.setBackground(Color.BLACK);
+                JButton b9 = (JButton) boardPanel.getComponent(8);
+                b9.setBackground(Color.RED);
+            } else if (currentPlayer.equals("O")) {
+                JButton b1 = (JButton) boardPanel.getComponent(0);
+                b1.setBackground(Color.BLACK);
+                JButton b2 = (JButton) boardPanel.getComponent(1);
+                b2.setBackground(Color.BLUE);
+                JButton b3 = (JButton) boardPanel.getComponent(2);
+                b3.setBackground(Color.BLACK);
+                JButton b4 = (JButton) boardPanel.getComponent(3);
+                b4.setBackground(Color.BLUE);
+                JButton b5 = (JButton) boardPanel.getComponent(4);
+                b5.setBackground(Color.BLACK);
+                JButton b6 = (JButton) boardPanel.getComponent(5);
+                b6.setBackground(Color.BLUE);
+                JButton b7 = (JButton) boardPanel.getComponent(6);
+                b7.setBackground(Color.BLACK);
+                JButton b8 = (JButton) boardPanel.getComponent(7);
+                b8.setBackground(Color.BLUE);
+                JButton b9 = (JButton) boardPanel.getComponent(8);
+                b9.setBackground(Color.BLACK);
+
+                JButton winButton = (JButton) boardPanel.getComponent(5);
+                Font winSymbol = new Font(winButton.getFont().getName(), Font.PLAIN, 400);
+                winButton.setFont(winSymbol);
+            }
+        }
+    }
+
+//Sets up the GUI and the games funcionality
+    static void display() {
+
+
+        setInitial(wholeBoardCheck);
+
+        Board tttBoard = new Board();
+        createBoard(tttBoard.getBoard());
+
+
+        JFrame wholeBoard = new JFrame("Ultimate Tic-Tac-Toe    Current Player: X");
+        wholeBoard.setFocusable(true); // Add this line to make the frame receive keyboard input
+        wholeBoard.setSize(1200, 1500);
+        //wholeBoard.setLayout(null);
+        wholeBoard.setVisible(true);
+
+
+
+        GridLayout gridLayout = new GridLayout(3, 3);
+        
+        wholeBoard.setLayout(gridLayout);
+        Border boardborder = BorderFactory.createDashedBorder(Color.BLACK, 4, 4, 4, false);
+        Border wonBorder = BorderFactory.createLineBorder(Color.RED, 10);
+
+
+
+
+        for (int board = 0; board < 9; board++) {
+            JPanel singleBoard = new JPanel();
+            singleBoard.setLayout(gridLayout);
+            singleBoard.setBorder(boardborder);
+            singleBoard.setVisible(true);
+
+            JButton[][] buttons = new JButton[3][3];
+
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    final int row = i;
+                    final int col = j;
+                    final int boardNumber = board;
+                    JButton b = new JButton("-");
+                    b.setVisible(true);
+                    
+                    b.addActionListener(new ActionListener() {
+                        public void actionPerformed(ActionEvent e) {
+
+                            if (!checkWinner(tttBoard.getBoard(boardNumber + 1))) {
+
+
+                                System.out.println("Row: " + row + " Col: " + col + " in Board " + boardNumber);
+
+                                if (playGame(boardNumber, row, col, currentPlayer, tttBoard, focusedBoard)) {
+                                    markBoard(boardNumber, row, col, wholeBoard);
+                                    if (wonBoards[boardNumber] == false && !checkWinner(tttBoard.getBoard(row * 3 + col+ 1))) {
+                                        changeFocus(row * 3 + col, wholeBoard);
+                                    } else {
+                                        changeFocus(-1, wholeBoard);
+                                    }
+                                    if (checkWinner(tttBoard.getBoard(boardNumber + 1))) {
+                                        wonBoards[boardNumber] = true;
+
+
+                                        System.out.println(boardNumber);
+                                        final int row = (boardNumber) / 3;
+                                        final int col = (boardNumber) % 3;
+                                        System.out.print(row);
+                                        System.out.print(col);
+
+                                        Board[][] wholeBoardSymbol = wholeBoardCheck.getBoard();
+
+
+                                        wholeBoardSymbol[row][col].setSym(currentPlayer);
+                                        
+
+                                        markWonBoard(boardNumber, wholeBoard);
+                                        JPanel wonPanel = (JPanel) wholeBoard.getContentPane().getComponent(boardNumber);
+                                        wonPanel.setBorder(wonBorder);
+
+                                    }
+
+                                         if(checkWinner(wholeBoardCheck)){
+
+                                            System.out.println(currentPlayer + " WINS THE GAME");
+                                            wholeBoard.setTitle("Ultimate Tic-Tac-Toe    "+currentPlayer+" WINS THE GAME!");
+                                             changePlayer();
+                                        }
+                                        else{
+                                    changePlayer();
+                                    wholeBoard.setTitle("Ultimate Tic-Tac-Toe    Current Player: "+currentPlayer);
+                                        }
+
+
+
+
+                                };
+
+
+                            } else {
+                                System.out.println("Board is won, cannot play in this board");
+                            }
+
+                        }
+                    });
+
+                    buttons[i][j] = b;
+                    singleBoard.add(buttons[i][j]);
+                }
+            }
+            wholeBoard.add(singleBoard); // Add the current singleBoard to the wholeBoard
+        }
+        wholeBoard.setVisible(true);
+
+
+    }
+
+
+}
